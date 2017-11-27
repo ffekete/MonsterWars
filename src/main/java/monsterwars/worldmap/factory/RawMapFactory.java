c25f3c4bd3133665f58614b887ba67d0a3359eb0
Ferenc Fekete <v-ffekete@hotels.com>
2017-11-27 11:30:28 +0100

    Refactored worldmap package factory classes.

--- src/main/java/monsterwars/worldmap/factory/RawMapFactory.java
+++ src/main/java/monsterwars/worldmap/factory/RawMapFactory.java
@@ -8,8 +8,15 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+/**
+ * Creates a {@link ConcurrentMap} instance for {@link monsterwars.worldmap.creator.TownMapCreator}.
+ */
 public class RawMapFactory {
 
+    /**
+     * Creates the {@link ConcurrentMap} instance.
+     * @return created map.
+     */
     public ConcurrentMap<Town, ConcurrentMap<Directions, Town>> create() {
         return new ConcurrentHashMap<>();
     }
--- src/main/java/monsterwars/worldmap/factory/TownDirectionsMapFactory.java
+++ src/main/java/monsterwars/worldmap/factory/TownDirectionsMapFactory.java
@@ -6,8 +6,15 @@ import monsterwars.worldmap.data.Town;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+/**
+ * Creates a {@link ConcurrentMap} instance for {@link monsterwars.worldmap.creator.TownMapCreator}.
+ */
 public class TownDirectionsMapFactory {
 
+    /**
+     * Creates the {@link ConcurrentMap} instance.
+     * @return created map.
+     */
     public ConcurrentMap<Directions, Town> create() {
         return new ConcurrentHashMap<>();
     }
